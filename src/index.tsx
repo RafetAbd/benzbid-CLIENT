dc85c121bf66ca53f3074dff0558aafd25b1fb9f
Rafet Abdalgalil <rafetalani@gmail.com>
2022-04-28 13:49:23 -0700

    added socket io to client

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,20 @@ import store from './store';
 import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import history from './history'
+import io from 'socket.io-client';
 // import './index.css';
 import App from './App';
+
+const socket = io('http://localhost:1337',
+{
+  transports: ['websocket'],
+  upgrade: false
+  });
+
+socket.on('connect', () => {
+  console.log('connected to server');
+
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
